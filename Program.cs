eb87c840dac1d789a3885efce5a243c3d3c86ffc
Maurizio Bramè <86922159+MauriBR@users.noreply.github.com>
2023-04-12 16:50:17 +0200

    Commit test codes

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SelTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var selTest = new SeleniumAutomation();
+            selTest.NewsLetter();
+            selTest.SignIn();
+
+            Console.ReadLine();
+        }
+    }
+}
